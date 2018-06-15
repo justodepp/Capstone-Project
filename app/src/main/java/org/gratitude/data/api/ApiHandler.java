7ee6abf052a756a828fd582bfd2f3c407d3e71dc
Gianni <gianni.cavalli@dshare.com>
2018-06-15 12:43:13 +0200

    fix ApiHandler Interceptor

--- app/src/main/java/org/gratitude/data/api/ApiHandler.java
+++ app/src/main/java/org/gratitude/data/api/ApiHandler.java
@@ -1,13 +1,10 @@
 package org.gratitude.data.api;
 
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import org.gratitude.Gratitude;
 import org.gratitude.utils.Params;
 import org.gratitude.utils.Utility;
 
@@ -27,25 +24,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiHandler {
 
-    private static final Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-
-            Response originalResponse = chain.proceed(chain.request());
-            if (isNetworkAvailable()) {
-                int maxAge = 60; // read from cache for 1 minute
-                return originalResponse.newBuilder()
-                        .header("Cache-Control", "public, max-age=" + maxAge)
-                        .build();
-            } else {
-                int maxStale = 60 * 60 * 24 * 28; // tolerate 2-weeks stale
-                return originalResponse.newBuilder()
-                        .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
-                        .build();
-            }
-        }
-    };
-
     public static ApiInterfaces getApiService(Context context) {
         return getApiService(context, true, Params.ENDPOINT);
     }
@@ -54,12 +32,30 @@ public class ApiHandler {
         return getApiService(context, converter, Params.ENDPOINT);
     }
 
-    private static ApiInterfaces getApiService(Context context, boolean useConverter, String ENDPOINT) {
+    private static ApiInterfaces getApiService(final Context context, boolean useConverter, String ENDPOINT) {
 
         int cacheSize = 10 * 1024 * 1024; // 10 MiB
-        OkHttpClient okHttpClient = new OkHttpClient.Builder().cache(new Cache(context.getCacheDir(), cacheSize))
-        .addInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR)
-        .build();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .cache(new Cache(context.getCacheDir(), cacheSize))
+                .addInterceptor(new Interceptor() {
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+
+                        Response originalResponse = chain.proceed(chain.request());
+                        if (Utility.isNetworkAvailable(context)) {
+                            int maxAge = 60; // read from cache for 1 minute
+                            return originalResponse.newBuilder()
+                                    .header("Cache-Control", "public, max-age=" + maxAge)
+                                    .build();
+                        } else {
+                            int maxStale = 60 * 60 * 24 * 28; // tolerate 2-weeks stale
+                            return originalResponse.newBuilder()
+                                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                                    .build();
+                        }
+                    }
+                })
+                .build();
 
         /*OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
             @Override
@@ -88,18 +84,4 @@ public class ApiHandler {
 
         return retrofitBuilder.build().create(ApiInterfaces.class);
     }
-
-    private boolean isNetworkAvailable(){
-        ConnectivityManager connMgr = (ConnectivityManager)
-                Gratitude.getApplication().getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        if (connMgr != null) {
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-            return (networkInfo != null && networkInfo.isConnected());
-        }
-        else
-            return false;
-    }
-
 }
