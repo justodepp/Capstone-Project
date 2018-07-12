81ec1dee87effdc5c3e4fb9088754f4369e60269
Gianni <gianni.cavalli@dshare.com>
2018-07-12 14:29:22 +0200

    wip widget

--- app/src/main/java/org/gratitude/ui/widget/GratitudeWidget.java
+++ app/src/main/java/org/gratitude/ui/widget/GratitudeWidget.java
@@ -1,10 +1,12 @@
 package org.gratitude.ui.widget;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
 
@@ -13,9 +15,9 @@ import com.bumptech.glide.request.target.AppWidgetTarget;
 import org.gratitude.R;
 import org.gratitude.data.db.GratitudeDatabase;
 import org.gratitude.data.model.projects.Project;
+import org.gratitude.main.MainActivity;
 import org.gratitude.ui.ProjectsFragment;
 import org.gratitude.ui.detailProject.DetailsProjectFragment;
-import org.gratitude.utils.AppExecutors;
 import org.gratitude.utils.GlideApp;
 
 import java.util.ArrayList;
@@ -28,25 +30,32 @@ public class GratitudeWidget extends AppWidgetProvider {
 
     private GratitudeDatabase mDb;
     private ArrayList<Project> projects;
-    private int chooseRandomIndex;
+    private int randomIndex;
 
+    @SuppressLint("StaticFieldLeak")
     @Override
     public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
 
         mDb = GratitudeDatabase.getInstance(context);
 
-        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+        // create new thread for querying database
+        new AsyncTask<Void, Void, Integer>(){
             @Override
-            public void run() {
+            protected Integer doInBackground(Void... voids) {
                 projects = new ArrayList<>(mDb.projectDao().loadAllWidgetProjects());
+                return projects.size();
+            }
+
+            @Override
+            protected void onPostExecute(Integer integer) {
 
                 // There may be multiple widgets active, so update all of them
                 for (int appWidgetId : appWidgetIds) {
 
                     // for each widget, create a random index...
-                    chooseRandomIndex = ThreadLocalRandom.current().nextInt(0, projects.size());
+                    randomIndex = ThreadLocalRandom.current().nextInt(0, projects.size());
                     // ... and then a random building
-                    Project project = projects.get(chooseRandomIndex);
+                    Project project = projects.get(randomIndex);
                     // intent and click listener
                     Intent intent = new Intent(context, DetailsProjectFragment.class);
                     intent.putExtra(ProjectsFragment.PRJ_CLICKED, project);
@@ -55,23 +64,27 @@ public class GratitudeWidget extends AppWidgetProvider {
                     // but also the back and up stack that should be inserted into the task
                     PendingIntent pendingIntent = TaskStackBuilder
                             .create(context)
-                            .addNextIntentWithParentStack(intent)
+                            .addParentStack(MainActivity.class)
+                            .addNextIntent(intent)
                             .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
                     RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.gratitude_widget);
                     views.setOnClickPendingIntent(R.id.layout, pendingIntent);
 
-                    AppWidgetTarget appWidgetTarget = new AppWidgetTarget( context, R.id.appwidget_image, views, appWidgetIds );
+                    AppWidgetTarget appWidgetTarget = new AppWidgetTarget( context, 96, 96, R.id.appwidget_image, views, appWidgetIds );
 
                     views.setTextViewText(R.id.appwidget_text, project.getTitle());
-                    GlideApp.with(context)
-                            .asBitmap()
-                            .load(project.getImageLink())
-                            .error(R.drawable.global_logo_color)
-                            .into(appWidgetTarget);
+
+                        GlideApp.with(context)
+                                .asBitmap()
+                                .load(project.getImageLink())
+                                .error(R.drawable.global_logo_color)
+                                .into(appWidgetTarget);
+
                     appWidgetManager.updateAppWidget(appWidgetId, views);
                 }
+
             }
-        });
+        }.execute();
     }
 }
 
